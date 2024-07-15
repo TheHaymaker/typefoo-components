a1583c304ef2dba5131664db61d7480cfb312727
Brett Haymaker <brett.haymaker@gmail.com>
2024-07-15 04:30:26 -0500

    typefoo

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
-import { FirstComponent } from "@/src/FirstComponent";
+import { FirstComponent } from "@/src";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <FirstComponent />
+      <FirstComponent formAction='dfd' href="/" variant='solid'>
+        hello there
+      </FirstComponent>
     </main>
   );
 }
--- src/FirstComponent.tsx
+++ src/FirstComponent.tsx
@@ -1,7 +1,37 @@
-const FirstComponent = () => (
-    <div>
-        First component
-    </div>
-)
+import React, { AnchorHTMLAttributes, ButtonHTMLAttributes, DetailedHTMLProps } from "react"
+
+type CommonProps = {
+    variant: 'solid' | 'outlined' | 'ghost'
+}
+type OGLinkProps = DetailedHTMLProps<AnchorHTMLAttributes<HTMLAnchorElement>, HTMLAnchorElement>
+type OGButtonProps = DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>
+type CommonAttributes = Pick<OGLinkProps | OGButtonProps, keyof OGLinkProps & keyof OGButtonProps>;
+type UniqueLinkProps = Omit<OGLinkProps, keyof CommonAttributes>
+type UniqueButtonProps = Omit<OGButtonProps, keyof CommonAttributes>
+
+type ThrowTypeErrorWhenUsedAsProps<Type> = {
+    [Property in keyof Type]?: never;
+};
+
+type LinkProps = OGLinkProps & ThrowTypeErrorWhenUsedAsProps<UniqueButtonProps>
+type ButtonProps = OGButtonProps & ThrowTypeErrorWhenUsedAsProps<UniqueLinkProps>
+type ComponentProps = CommonProps & (LinkProps | ButtonProps) & React.PropsWithChildren
+
+const isLink = (el: LinkProps | ButtonProps): el is LinkProps => {
+    return (el as LinkProps).href !== undefined;
+}
+
+const FirstComponent = ({ variant, ...props }: ComponentProps) => {
+    if (isLink(props)) {
+        const { href, children, ...rest } = props
+        return <a href={href} {...rest}>{`I am a ${variant} link`}</a>
+    } else {
+        const { children, ...rest } = props
+        return (<button {...rest}>
+            {children}
+            I am a button {`${variant}`}
+        </button>)
+    }
+}
 
 export default FirstComponent
